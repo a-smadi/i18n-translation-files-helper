4d9eec0db7058b3dc6493dc447a9e409491ef8ca
Ali Smadi <alielsmadi@gmail.com>
2018-01-26 13:24:50 +0200

    fixed evaluation of english language in value when numbers are included

--- string_ops.rb
+++ string_ops.rb
@@ -1,5 +1,5 @@
 class StringOps
-  ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z].freeze
+  ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9].freeze
 
   def self.underscore(text)
     text.gsub(/::/, '/').gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').gsub(/([a-z\d])([A-Z])/, '\1_\2').tr('-', '_').downcase
