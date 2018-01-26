42aadb224d881f373ff2ba487989a20cbc75a5de
Ali Smadi <alielsmadi@gmail.com>
2018-01-26 14:37:43 +0200

    improved language detection accuracy

--- string_ops.rb
+++ string_ops.rb
@@ -1,14 +1,21 @@
 class StringOps
-  ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9].freeze
+  ALPHABET = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z].freeze
 
   def self.underscore(text)
     text.gsub(/::/, '/').gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').gsub(/([a-z\d])([A-Z])/, '\1_\2').tr('-', '_').downcase
   end
 
   def self.english?(text)
-    text = text.tr(' ', '')
+    text = letters_only(text)
+    return true unless text.present? && !text.empty?
     score = 0
-    text.chars.each { |char| score += (100.0 / text.size) if ALPHABET.include?(char.downcase) }
-    score >= 75
+    text.chars.each { |char| score += (100.0 / text.size) if ALPHABET.include?(char) }
+    score >= 50
+  end
+
+  private_class_method
+
+  def self.letters_only(text)
+    text.gsub(/[^0-9A-Za-z]/, '').tr('0-9', '').strip.downcase
   end
 end
